ff5de3f74e3fe5d55a2ba390082cbdcc6ab95311
patriciadunkelman <paddylin2005@gmail.com>
2025-04-15 17:22:12 -0400

    added a course class, with getters and setters as well as documentation for get_schedule and visualized_schedule

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,64 @@
-test
+class Course:
+    def __init__(self, name, id, times, credits):
+        self.name = name
+        self.id = id
+        self.times = times
+        self.credits = credits
+
+    #getters and setters
+    def get_name(self):
+        return self.name
+
+    def get_id(self):
+        return self.id
+
+    def get_times(self):
+        return self.times
+
+    def get_credits(self):
+        return self.credits
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_id(self, id):
+        self.id = id
+
+    def set_times(self, times):
+        self.times = times
+
+    def set_credits(self, credits):
+        self.credits = credits
+
+    def __str__(self):
+        return f"Course Name: {self.name}, Class ID: {self.id}, Credits: {self.credits}, Times: {self.times}"
+
+    @classmethod
+    def get_schedule(cls, classes: list, preference: str):
+        """
+        Function: the function returns a schedule of the entered classes, prioritizing classes listed first and keeping in mind the preferences of early/ midday/ or late classes.
+
+        Parameters:
+            classes (list): the list of classes to schedule.
+            preference (str): the preference of early / midday or late classes.
+
+        Returns:
+            schedule (list): the schedule of the entered classes.
+        """
+        schedule = []
+        return schedule
+
+    @classmethod
+    def visualize_schedule(cls, classes: list):
+        """
+        Function: the function returns a visual representation of the schedule.
+
+        Parameters:
+            classes (list): the schedule of the entered classes.
+
+        Returns:
+            schedule (list): the visual representation of the schedule.
+        """
+
+        schedule = []
+        return schedule
