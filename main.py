a2f2d8c940ffbef0957444fa9a84a118a8a19e90
jescandor1 <jescandor1@ufl.edu>
2025-04-16 13:24:00 -0400

    added load_courses function that converts data into Course objects

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import json
+import scheduler
 
 sample_data = [
     {"name": "Computational Math", "id": "MAD 2502", "periods": [1, 2, 3], "credits": 3},
+    {"name": "Calculus 3", "id": "MAC 2313", "periods": [3, 4, 5], "credits": 4}
 ]
 #writes json file
 with open("data.json", "w") as f:
     json.dump(sample_data, f, indent=4)
 
+def load_courses(filename):
+    with open(filename, "r") as infile:
+        data = json.load(infile)
+        courses = [
+            scheduler.Course(
+                record["name"],
+                record["id"],
+                record["periods"],
+                record["credits"]
+            )
+            for record in data
+        ]
+    return courses
+
 
 def main():
     filename = "data.json"
 
-    with open(filename, "r") as infile:
-        data = json.load(infile)
+    courses = load_courses(filename)
 
     # print test
-    for record in data:
-        name = record.get("name", "N/A")
-        student_id = record.get("id", "N/A")
-        periods = record.get("periods", [])
-        credit = record.get("credits", "N/A")
-
-        print(f"Name: {name}")
-        print(f"ID: {student_id}")
-        print(f"Periods: {periods}")
-        print(f"Credits: {credit}")
+    for course in courses:
+        print(course)
 
 
 if __name__ == '__main__':
