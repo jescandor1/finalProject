a418ec48e8822f9b0e69153e21a9dc49405f937f
jescandor1 <jescandor1@ufl.edu>
2025-04-23 21:10:49 -0400

    modified json data to accomodate for different sections

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import json
 import scheduler
 
 sample_data = [
-    {"name": "Computational Math", "id": "MAD 2502", "periods": ["M1", "W2", "F3"], "credits": 3},
-    {"name": "Calculus 3", "id": "MAC 2313", "periods": ["M1", "R2", "F3"], "credits": 4},
-    {"name": "Programming With Data in R", "id": "STA 3100", "periods": ["M5", "T7", "W8"], "credits": 3},
-    {"name": "Computational Linear Algebra", "id": "MAS 3114", "periods": ["T5", "R5"], "credits": 3}
+    {"name": "Computational Math", "id": "MAD 2502", "periods": (["M5", "W5", "F5"]), "credits": 3},
+    {"name": "Calculus 3", "id": "MAC 2313", "periods": (["M2", "T2", "W2", "F2"], ["M6", "W6", "R11", "F6"], ["M8", "T4", "W8", "F8"]), "credits": 4},
+    {"name": "Programming With Data in R", "id": "STA 3100", "periods": (["M5", "W5", "F5"], ["M7", "W7", "F7"], ["M8", "W8", "F8"]), "credits": 3},
+    {"name": "Computational Linear Algebra", "id": "MAS 3114", "periods": (["M5", "W5", "F5"], ["O"]), "credits": 3}
 ]
 #writes json file
 with open("data.json", "w") as f:
